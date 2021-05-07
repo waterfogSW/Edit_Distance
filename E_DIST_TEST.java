2bc30e9df9994703ba22a726f064725f4581ce68
waterfog9580 <waterfog9580@gmail.com>
2021-05-07 16:35:58 +0900

    update

--- E_DIST_TEST.java
+++ E_DIST_TEST.java
@@ -11,7 +11,6 @@ public class E_DIST_TEST {
 
         E_DIST E = new E_DIST(str1,str2);
         System.out.println("Min cost : " + E.editDistDP(str1,str2));
-        
         System.out.println("----------");
         for (int i = 0; i < E.cost.length; i++) {
             for (int j = 0; j < E.cost[i].length; j++) {
