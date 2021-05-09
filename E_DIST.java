2a56506bd0fa926a4a0acee62bb3842ce27ca3ac
waterfogSW <waterfog9580@gmail.com>
2021-05-09 15:56:17 +0900

    update

--- E_DIST.java
+++ E_DIST.java
@@ -10,15 +10,19 @@ public class E_DIST {
         m = _str1.length();
         n = _str2.length();
         cost = new int [m+1][n+1];      // Minimum Cost
+        edit = new char [m+1][n+1];     // Edit Order
     }
 
-    private int minEdit(int ins, int chg, int del) {
-        if(ins <= chg && ins <= del) {
-            return ins;
-        } else if(chg <= ins && chg <= del) {
+    private int minEdit(int del, int chg, int ins,int i, int j) {
+        if(del < chg && del < ins) {
+            edit[i][j] = 'D';
+            return del;
+        } else if(chg < ins && chg < del) {
+            edit[i][j] = 'C';
             return chg;
         } else {
-            return del;
+            edit[i][j] = 'I';
+            return ins;
         }
     }
 
@@ -27,20 +31,52 @@ public class E_DIST {
             for (int j = 0; j <= n; j++) {
                 if (i == 0) {
                     cost[i][j] = j;
+                    edit[i][j] = 'I';
                 }
                 else if(j == 0) {
                     cost[i][j] = i;
+                    edit[i][j] = 'D';
                 }
                 else if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
                     cost[i][j] = cost[i - 1][j - 1];
+                    edit[i][j] = 'C';
                 }
 
                 else
                     cost[i][j] = minEdit(cost[i-1][j] + 1,
-                                    cost[i-1][j-1] + 2,
-                                    cost[i][j - 1] + 1);
+                                        cost[i-1][j-1] + 2,
+                                        cost[i][j - 1] + 1,
+                                        i,j);
             }
         }
+        edit[0][0] = '-';
         return cost[m][n];
+    }
+    public char[] optOrder() {
+        int i = m;
+        int j = n;
+        int count = 0;
+        char c_tmp = edit[i][j];
+        char temp[] = new char[m+n];
+        while(i != 0 || j != 0) {
+            if (edit[i][j] == 'I'){
+                c_tmp = 'I';
+                j--;
+            } else if (edit[i][j] == 'D') {
+                c_tmp = 'D';
+                i--;
+            } else {
+                c_tmp = 'C';
+                i--;
+                j--;
+            }
+            temp[count++] = c_tmp;
+        }
+
+        char order[] = new char[count];
+        for(int k = 0; k < count; k++) {
+            order[k] = temp[count - k - 1];
+        }
+        return order;
     }
 }
--- E_DIST_TEST.java
+++ E_DIST_TEST.java
@@ -11,12 +11,28 @@ public class E_DIST_TEST {
 
         E_DIST E = new E_DIST(str1,str2);
         System.out.println("Min cost : " + E.editDistDP());
-        System.out.println("----------");
+        System.out.println("-----Cost Table-----");
         for (int i = 0; i < E.cost.length; i++) {
             for (int j = 0; j < E.cost[i].length; j++) {
                 System.out.print(E.cost[i][j] + " ");
             }
             System.out.println();
         }
+        System.out.println("--------------------");
+        System.out.println("-----Edit Table-----");
+        for (int i = 0; i < E.edit.length; i++) {
+            for (int j = 0; j < E.edit[i].length; j++) {
+                System.out.print(E.edit[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------");
+
+        char order[] = E.optOrder();
+        System.out.print("Edit Order : ");
+        for (int i = 0; i < order.length - 1; i++) {
+            System.out.print(order[i] + "->");
+        }
+        System.out.println(order[order.length-1]);
     }
 }
